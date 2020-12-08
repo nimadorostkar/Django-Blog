3557165600910a22523b14e233c4bfba63780466
nima <54316106+nimadorostkar@users.noreply.github.com>
2020-12-08 15:45:57 +0330

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
 STATUS = (
     (0,"Draft"),
     (1,"Publish")
 )
  
+    
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
